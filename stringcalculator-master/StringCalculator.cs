0719d2cbfdc85f30e490cf0d2ee6ce755a2e5047
Michael Mitsch <mfmitsch@gmail.com>
2022-09-13 17:21:47 -0400

    add comments to code

--- stringcalculator-master/StringCalculator.cs
+++ stringcalculator-master/StringCalculator.cs
@@ -8,15 +8,19 @@ public class StringCalculator
     public int Add(string numbers)
     {
         string[] splitNums;
+        // check for custom delimeter
         if (numbers.StartsWith("//") && numbers.Length >= 4
             && numbers.ElementAt(3) == '\n') 
         {
+            //split by normal and custom delimeters
             char newDelim = numbers.ElementAt(2);
             numbers = numbers.Remove(0, 4);
             splitNums = numbers.Split(',','\n',newDelim);
         }
+        //otherwise splite by normal delimeters
         else splitNums = numbers.Split(',', '\n');
         var total = 0;
+        // add up each number and return zero if you encounter non int
         foreach(var strNum in splitNums)
         {
             if(int.TryParse(strNum,out var intNum))
